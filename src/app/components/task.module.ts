0f033bd1ba5acfc7a1822169ea38b6abebd7b341
kevin quing <willardquing@gmail.com>
2021-08-22 14:33:44 +0800

    updates: https://storybook.js.org/tutorials/intro-to-storybook
    * Screens

--- src/app/components/task.module.ts
+++ src/app/components/task.module.ts
@@ -5,11 +5,19 @@ import { TasksState } from '../state/task.state';
 import { TaskComponent } from './task/task.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { PureTaskListComponent } from './pure-task-list/pure-task-list.component';
+// import { PureInboxScreenComponent } from './pure-inbox-screen/pure-inbox-screen.component';
+// import { InboxScreenComponent } from './inbox-screen/inbox-screen.component';
 
 
 
 @NgModule({
-  declarations: [TaskComponent, TaskListComponent, PureTaskListComponent],
+  declarations: [
+    TaskComponent,
+    TaskListComponent,
+    PureTaskListComponent,
+    // PureInboxScreenComponent,
+    // InboxScreenComponent
+  ],
   imports: [
     CommonModule,
     NgxsModule.forFeature([TasksState])
